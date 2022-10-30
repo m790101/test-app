d7eb9cea48da8d457d7b9dbd71018ff9b23ea726
m790101 <m790101@hotmail.com>
2022-10-30 22:15:05 +0800

    router change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import { Routes,BrowserRouter, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Test from './pages/Test'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import '../src/main.scss'
 const App = () => {
   return (
-    <BrowserRouter>
+    <div>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/test" element={<Test/>}/>
       <Route path='*' element={<NotFound/>}/>
     </Routes>
-    </BrowserRouter>
-    
+    </div>
   );
 }
 
